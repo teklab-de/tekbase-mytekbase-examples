2c792d27ed4c128e315730500e5f68550f4639f5
TekLab <teklab@noreply.gitgem.com>
2018-08-03 23:38:13 +0200

    'mytekbase/members/modules/games/xxx/dbcreate_module.php' ändern

--- mytekbase/members/modules/games/xxx/dbcreate_module.php
+++ mytekbase/members/modules/games/xxx/dbcreate_module.php
@@ -18,22 +18,22 @@ if ($urow == 0) {
 	$databaseuser = "".$memstats[member]."_".$databaseuser."";
 	$statuscode = tekwebexec($rsstats[sshdaemon], $rsstats[daemonpasswd], $rsstats[sshport], $rsstats[sshuser], $rsstats[path], $options[serverip], 'dbcreate', $memstats[member], $databasename, $databaseuser, $memstats[ftppasswd], '', '', '', '', '');
 	if ($statuscode == "error_00001") {
-		$membermsgtwo = member_errorback(""._SSHSERVEROFFLINE."");
+		$membermsgtwo = member_errorback(_SSHSERVEROFFLINE);
 	}
 	if ($statuscode == "error_00002") {
-		$membermsgtwo = member_errorback(""._SSHKEYERROR."");
+		$membermsgtwo = member_errorback(_SSHKEYERROR);
 	}
 	if ($statuscode == "error_00003") {
-		$membermsgtwo = member_errorback(""._SSHBADLOGIN."");
+		$membermsgtwo = member_errorback(_SSHBADLOGIN);
 	}
 	if ($statuscode == "error_00004") {
-		$membermsgtwo = member_errorback(""._SSHDAEMONBADLOGIN."");
+		$membermsgtwo = member_errorback(_SSHDAEMONBADLOGIN);
 	}
 	if ($statuscode == "error_00007" OR $statuscode == "error_00008") {
-		$membermsgtwo = member_errorback(""._DBCREATEINSTALLERROR."");
+		$membermsgtwo = member_errorback(_DBCREATEINSTALLERROR);
 	}
 	if ($statuscode == "error_00009") {
-		$membermsgtwo = member_errorback(""._SSHUSERERROR."");
+		$membermsgtwo = member_errorback(_SSHUSERERROR);
 	}
 	if ($statuscode == "ok_00001") {
 		$result = $db->sql_query("INSERT INTO ".$prefix."_databases (id, memberid, productid, serverip, name, login, rserverid) VALUES (NULL, '$memstats[id]', '$options[productid]', '$options[serverip]', '$databasename', '$databaseuser', '$rsstats[id]')");
@@ -41,8 +41,8 @@ if ($urow == 0) {
 		die();
 	}else{
 		include ("members/header.php");
-		$membertitle = ''._GSERVER.'&nbsp;-&nbsp;'.$options[serverip].':'.$options[serverport].'';
-		$membermsg = member_title("gserver", $membertitle, ""._ASSISTENTGSERVER."", "");
+		$membertitle = _GSERVER.'&nbsp;-&nbsp;'.$options[serverip].':'.$options[serverport].'';
+		$membermsg = member_title("gserver", $membertitle, _ASSISTENTGSERVER, "");
 		echo ''.$membermsg.'
 			  '.$membermsgtwo.'';
 		include ("members/footer.php");				  
