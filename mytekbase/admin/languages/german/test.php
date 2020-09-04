047b06c3c230f9c555bc0e125d36acda784fd6eb
Ch.Frankenstein <ch.frankenstein@googlemail.com>
2020-09-05 00:20:05 +0200

    Create test.php

---
+++ mytekbase/admin/languages/german/test.php
@@ -0,0 +1,9 @@
+define("_TESTSINGULAR","Test Modul");
+define("_TESTPLURAL","Test Module");
+define("_TESTTHESINGULAR","Das");
+
+define("_TEST","Test Modul");
+define("_TESTXXXX","XXXX");
+define("_TESTYYYY","YYYY");
+
+define("_ASSISTENTTEST","Hilfe zum Modul...");
