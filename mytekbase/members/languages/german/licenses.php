9cc09c2a13cf6eda5fcfb25fb89900a3119fb83f
Ch.Frankenstein <ch.frankenstein@googlemail.com>
2021-01-27 17:38:40 +0100

    Create licenses.php
    License management for reseller customers - german language file

---
+++ mytekbase/members/languages/german/licenses.php
@@ -0,0 +1,24 @@
+<?php
+
+///////////////////////////////////////////
+// Licenses
+////////////////////////////////////////////
+define("_LICENSES","Lizenzen");
+define("_LICENSESNOENTRY","<b>Keine Lizenzen vorhanden!</b>");
+define("_LICENSESNOTALLFIELDS","Es wurden keine IP/Domain ausgewählt!");
+define("_LICENSESMAINTENANCE","Der Lizenzserver ist zurzeit nicht erreichbar bitte versuchen Sie es in 1-2 Minuten noch einmal!");
+define("_LICENSESURLIP","Domain oder IP:");
+define("_LICENSESURL","Freigeschaltet:");
+define("_LICENSESURLLOCK","Gesperrt:");
+define("_LICENSESRUNNINGURL","Die Lizenz läuft derzeit unter:");
+define("_LICENSESDBUPERROR","Datenbankfehler! Die Lizenz konnte nicht aktiviert werden!");
+define("_LICENSESDBUPDATED","Die Lizenz wurde soeben aktiviert!");
+define("_LICENSESERROR","Die Lizenz ist gesperrt und kann daher nicht gewechselt werden!");
+define("_LICENSESCONNECT","Lizenz zuweisen");
+define("_TABLEFREEDATE","Updates bis");
+define("_TABLEURLIP","Domain oder IP");
+define("_TABLEPATH","Verzeichnis");
+define("_ASSISTENTLICENSES","Hier können Sie Ihre Lizenzen einsehen und auch ändern. Wenn Sie eine neue Lizenz aktivieren,
+wird die alte Lizenz automatisch gesperrt.");
+
+?>
